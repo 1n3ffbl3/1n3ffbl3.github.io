389dfcfb57ec57d1ff19a8379c9887c0f93354fb
1n3ffbl3 <mizgier.m@gmail.com>
2019-03-29 17:14:10 +0100

    Working on navigation bar

--- portfolio-1n3ffbl3/src/components/Header/Header.js
+++ portfolio-1n3ffbl3/src/components/Header/Header.js
@@ -1,10 +1,13 @@
 import styled from 'styled-components';
 
 const Header = styled.h1`
-	margin: 0;
-	font-size: 2em;
+	font-size: 1em;
 	font-family: 'Montserrat';
 	color: white;
+	flex-shirnk: 1;
+	flex-grow: 1;
+	margin-right: 200px;
+	letter-spacing: 1px;
 `;
 
 export default Header;
---
+++ portfolio-1n3ffbl3/src/components/Header/MainHeader.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const MainHeader = styled.h1`
+	font-size: 1em;
+	font-family: sans serif;
+	font-size: 10px;
+    letter-spacing: 2px;
+	text-transform: uppercase;
+	color: white;
+	text-align: center;
+	position: relative;
+	width: 100%;
+	height: 60px;
+	max-height: 100%;
+	min-height: unset;
+`;
+
+export default MainHeader;
