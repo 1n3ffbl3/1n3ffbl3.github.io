1eee6643b13d8aa5d70c93b9dbe71ea4c0f308ed
1n3ffbl3 <mizgier.m@gmail.com>
2019-04-09 23:06:24 +0200

    Changing css. Adding new photo

--- portfolio-1n3ffbl3/src/components/Projects/ProjectWrapper.js
+++ portfolio-1n3ffbl3/src/components/Projects/ProjectWrapper.js
@@ -4,17 +4,46 @@ import Card from './Card/CardWrapper';
 
 
 const ProjectsWrapper = styled.div`
+	display: flex;
+	flex-wrap: wrap;
 	padding: 40px 60px 20px 60px;
-	width: 100%;
-	height: 100vh;
-	background-color: blue;
-	background-size: cover;
-	background-repeat: no-repeat;
+	// width: 100%;
+	// height: 100vh;
+	// background-color: blue;
+	// background-size: cover;
+	// background-repeat: no-repeat;
 `;
+
+const DivClass = styled.div`
+	flex: 1 0 21%; 
+	margin: 5px;
+	// height: 100px;
+`;
+
 const ProjectWrapper = ({ children }) => (
 	<ProjectsWrapper>
-		Projects
-	<Card>{children}</Card>
+		{/* Projects */}
+		<DivClass >
+			<Card>{children}</Card>
+		</DivClass>
+		<DivClass>
+			<Card>{children}</Card>
+		</DivClass>
+		<DivClass>
+			<Card>{children}</Card>
+		</DivClass>
+		<DivClass>
+			<Card>{children}</Card>
+		</DivClass>
+		<DivClass>
+			<Card>{children}</Card>
+		</DivClass>
+		<DivClass>
+			<Card>{children}</Card>
+		</DivClass>
+		{/* <Card>{children}</Card>
+		<Card>{children}</Card>
+		<Card>{children}</Card> */}
 	</ProjectsWrapper>
 );
 
