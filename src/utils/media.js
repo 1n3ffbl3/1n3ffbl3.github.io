ff5841110e564ef8df5dc2c06e716258b4dfbdd2
1n3ffbl3 <mizgier.m@gmail.com>
2020-02-22 21:45:41 +0100

    Remove unused function.

--- src/utils/media.js
+++ src/utils/media.js
@@ -1,17 +1,6 @@
 import { css } from 'styled-components';
 import { ScreenSize } from './enums';
 
-const maxScreenWidth = Object.keys(ScreenSize).reduce((accumulator, currValue) => {
-    accumulator[currValue] = (...args) => css`
-        @media (max-width: ${ScreenSize[currValue]}) {
-            ${css(...args)}
-        }
-    `;
-    return accumulator;
-}, {});
-
-export default maxScreenWidth;
-
 const mobile = (...args) => {
     return css`
         @media only screen and (max-width:${ScreenSize.xs}px) {
